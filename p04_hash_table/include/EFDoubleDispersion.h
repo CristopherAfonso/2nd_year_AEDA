3c6e70d260b218b1eecaf93cf40718ffc236cbcc
CristopherAfonso <alu0101402031@ull.edu.es>
2022-04-04 17:02:20 +0100

    archivos de clases declarados

---
+++ p04_hash_table/include/EFDoubleDispersion.h
@@ -0,0 +1,39 @@
+/** 
+ * Universidad de La Laguna (ULL)
+ * Escuela Superior de Ingenieria y Tecnologia (ESIT)
+ * Grado en Ingenieria Informatica
+ * Asignatura: Algorítmos y Estructuras de Datos Avanzados
+ * Curso: 2º
+ * Practica 4: Implementación de Tabla Hash
+ * @author Cristopher Manuel Afonso Mora
+ * Correo: alu0101402031@ull.edu.es
+ * @date 03/04/2022
+ * 
+ * @file EFDoubleDispersion.h 
+ * @brief Donde se aloja la declaración de la clase EFDoubleDispersion y la
+ * declaracion de sus metodos. La clase EFDoubleDispersion es un tipo de clase
+ * hija de una clase abstracta, la cual define una forma peculiar de una
+ * funcion de exploracion, esta clase hija define la funcion de exploracion en
+ * base a una formula de doble dispersion, la cual consiste en aplicar dentro
+ * de una formula, otras dos que sumados sus resultados, se les aplica el
+ * modulo de una division y te da el resultado.
+ *
+ * @bug No hay bugs conocidos
+ * 
+ * Referencias: Enlaces de Interes
+ * @see 
+ * 
+ * @version 1.0
+ * @brief Historial de Revisiones 
+ * 03/04/22 - Creacion (primera version) del codigo:
+ *            Solo he creado el archivo, le he puesto el comentario de
+ *            cabecera y declarado los #include necesarios.
+ * 03/04/22 - Archivo terminado.
+ */
+
+#ifndef _EFDOUBLEDISPERSION_
+#define _EFDOUBLEDISPERSION_
+
+
+
+#endif
---
+++ p04_hash_table/include/EFReDispersion.h
@@ -0,0 +1,37 @@
+/** 
+ * Universidad de La Laguna (ULL)
+ * Escuela Superior de Ingenieria y Tecnologia (ESIT)
+ * Grado en Ingenieria Informatica
+ * Asignatura: Algorítmos y Estructuras de Datos Avanzados
+ * Curso: 2º
+ * Practica 4: Implementación de Tabla Hash
+ * @author Cristopher Manuel Afonso Mora
+ * Correo: alu0101402031@ull.edu.es
+ * @date 03/04/2022
+ * 
+ * @file EFReDispersion.h 
+ * @brief Donde se aloja la declaración de la clase EFReDispersion y la
+ * declaracion de sus metodos. La clase EFReDispersion es un tipo de clase
+ * hija de una clase abstracta, la cual define una forma peculiar de una
+ * funcion de exploracion, esta clase hija define la funcion de exploracion en
+ * base a una formula que elige funciones de exploracion de forma aleatoria.
+ *
+ * @bug No hay bugs conocidos
+ * 
+ * Referencias: Enlaces de Interes
+ * @see 
+ * 
+ * @version 1.0
+ * @brief Historial de Revisiones 
+ * 03/04/22 - Creacion (primera version) del codigo:
+ *            Solo he creado el archivo, le he puesto el comentario de
+ *            cabecera y declarado los #include necesarios.
+ * 03/04/22 - Archivo terminado.
+ */
+
+#ifndef _EFREDISPERSION_
+#define _EFREDISPERSION_
+
+
+
+#endif
