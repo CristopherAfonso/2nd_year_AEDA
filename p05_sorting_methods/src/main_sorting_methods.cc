e8d4c608f8aefed0239e512dc8d4d8a230328ae1
CristopherAfonsoMora <alu0101402031@ull.edu.es>
2022-04-22 00:20:03 +0100

    interfaz del programa terminada

---
+++ p05_sorting_methods/src/main_sorting_methods.cc
@@ -0,0 +1,40 @@
+/**
+ * Universidad de La Laguna (ULL)
+ * Escuela Superior de Ingenieria y Tecnologia (ESIT)
+ * Grado en Ingenieria Informatica
+ * Asignatura: Algorítmos y Estructuras de Datos Avanzados
+ * Curso: 2º
+ * Practica 5: Implementación de Métodos de Ordenación
+ * @author Cristopher Manuel Afonso Mora
+ * Correo: alu0101402031@ull.edu.es
+ * @date 21/04/2022
+ * 
+ * @file main_sorting_methods.cc (programa cliente)
+ * @brief Programa que nos permite aplicar a un vector dado por el usuario o
+ * generado aleatoriamente, uno de varios métodos de ordenacion famosos sobre
+ * él y mostrarlo por pantalla mientras se ordena para ver su desarrollo.
+ * 
+ * Si quiere saber mas detalles sobre como usar el programa ejecutable,
+ * ejecute: ./main_sorting_methods --help
+ * para mas informacion.
+ * 
+ * @bug No hay bugs conocidos
+ * 
+ * Referencias:
+ * @see
+ * 
+ * @version 1.0
+ * @brief Historial de Revisiones 
+ * 21/04/2022 - Creacion (primera version) del codigo:
+ *              Solo he creado el archivo, le he puesto el comentario de
+ *              cabecera y declarado los #include necesarios.
+ * 22/04/2022 - Archivo terminado.
+ */
+
+#include "../include/main_functions.h"
+
+int main(int argc, char* argv[]) {
+  Usage(argc, argv);
+  std::cout << "piola\n";
+  return 0;
+}
